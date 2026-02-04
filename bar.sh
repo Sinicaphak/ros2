bd7c2a770189c85e41f6e1ac9623eb17ab7dd530
jetson orin 廖俊煜 <2967306689@qq.com>
2026-02-04 20:42:21 +0800

    更新 readme.md 中的依赖需求

--- bar.sh
+++ bar.sh
@@ -3,6 +3,6 @@ root_path="/home/apollo/disk/ros2"
 cd $root_path
 colcon build --packages-select car mpc_planner
 source $root_path/install/setup.bash
-# clear
+clear
 # ros2 launch car car_launch.py
 ros2 launch mpc_planner gzaebo.launch.py
--- readme.md
+++ readme.md
@@ -1,4 +1,6 @@
 cd /home/apollo/disk/ros2/src
+pip install casadi
+sudo apt install ros-humble-osqp-vendor
 git clone https://github.com/agilexrobotics/ugv_sdk.git
 git clone https://github.com/agilexrobotics/hunter_ros2.git
 cd ..
